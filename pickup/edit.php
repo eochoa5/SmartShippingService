2c14abdb4dcc069cd8d29e27709a931cdb270d89
Ariel Suarez <ariel.suarez1994@gmail.com>
2016-11-22 20:46:36 -0800

    Update edit.php

--- pickup/edit.php
+++ pickup/edit.php
@@ -7,8 +7,17 @@ if(isset($_POST["first"])){
 	$phone=mysqli_real_escape_string($db_conx,$_POST["phone"]);
 	$email=mysqli_real_escape_string($db_conx,$_POST["email"]);
 	$id=mysqli_real_escape_string($db_conx,$_POST["id"]);
+	$date=date('Y-m-d',strtotime($_POST['dt']));
+	$time=date(($_POST['tm']));
+	if(!isset($_POST["date"])){
+		$sql = "DELETE FROM `pickups` WHERE `pickups`.`shipmentId` = '$id'";
+		$query = mysqli_query($db_conx, $sql);
+	}else{
+		$sql = "UPDATE pickups SET `first`='$first', `last`='$last', `phone`='$phone', `email`='$email', `date`='$date', `time`=$time WHERE `shipmentId`=$id ";
+		$query = mysqli_query($db_conx, $sql); 
+	}
 	
-	$sql = "UPDATE pickups SET `first`='$first', `last`='$last', `phone`='$phone', `email`='$email', `time`='4:00pm' WHERE `id`=$id ";
+	$sql = "UPDATE pickups SET `first`='$first', `last`='$last', `phone`='$phone', `email`='$email', `date`='$date', `time`=$time WHERE `shipmentId`=$id ";
 	$query = mysqli_query($db_conx, $sql); 
 	
 }
@@ -27,6 +36,8 @@ if(isset($_POST["first"])){
 <form id="editForm" name="editForm" method="post" action="edit.php">
 <div class="abc" style="margin-top:10px">Please fill out the information below</div>
  <div id="signupFormDiv">
+	<label><b>Shipment ID</b></label><br>
+    <input id="id" type="text" placeholder="ID" name="id" required maxlength="20" size="40"><br/>
     <label><b>First</b></label><br>
     <input id="first" type="text" placeholder="First" name="first" required maxlength="30"><br>
     <label><b>Last</b></label><br>
@@ -35,8 +46,6 @@ if(isset($_POST["first"])){
     <input id="phone" type="text" placeholder="Phone Number" name="phone" required maxlength="15"><br>
 	<label><b>Email</b></label><br>
     <input id="email" type="text" placeholder="Email" name="email" required maxlength="50"><br>
-	<label><b>Pick up ID</b></label><br>
-    <input id="id" type="text" placeholder="Pick up ID" name="id" required maxlength="20" size="40"><br/>
 	<label><b>New date (leave blank if cancelling)</b></label><br>
 	<input type="date" name="dt"><br/>
 	<label><b>New time (leave blank if cancelling)</b></label><br>

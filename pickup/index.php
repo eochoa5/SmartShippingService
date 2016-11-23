1a9a3d34d20cdb190cf1feec3d29594855462924
Ariel Suarez <ariel.suarez1994@gmail.com>
2016-11-23 12:42:59 -0800

    Update index.php

--- pickup/index.php
+++ pickup/index.php
@@ -3,7 +3,6 @@ if(isset($_POST["first"])){
 	include_once("../database/connect.php");
 	
 	$first=mysqli_real_escape_string($db_conx,$_POST["first"]);
-	$id =mysqli_real_escape_string($db_conx,$_POST["shipmentId"]);
 	$last=mysqli_real_escape_string($db_conx,$_POST["last"]);
 	$email=mysqli_real_escape_string($db_conx,$_POST["email"]);
 	$date=date('Y-m-d',strtotime($_POST['dt']));
@@ -11,9 +10,14 @@ if(isset($_POST["first"])){
 	$phone=mysqli_real_escape_string($db_conx,$_POST["phone"]);
 	$insurance=mysqli_real_escape_string($db_conx,$_POST["insurance"]);
 	$address=mysqli_real_escape_string($db_conx,$_POST["address"]);
+	$state=mysqli_real_escape_string($db_conx,$_POST["state"]);
+	$zip=mysqli_real_escape_string($db_conx,$_POST["zip"]);
+	$address2=mysqli_real_escape_string($db_conx,$_POST["address2"]);
+	$state2=mysqli_real_escape_string($db_conx,$_POST["state2"]);
+	$zip2=mysqli_real_escape_string($db_conx,$_POST["zip2"]);
 	
-	$sql = "INSERT INTO `pickups`(`id`, `shipmentId`, `first`, `last`, `type`, `email`,`date`, `phone`, `time`, `insurance`, `pickedUp`, `locationAddress`) 
-	VALUES ('', '$id', '$first','$last', 'Normal', '$email','$date', '$phone','$time','$insurance','No','$address')";
+	$sql = "INSERT INTO `pickups`(`id`, `first`, `last`, `type`, `email`,`date`, `phone`, `time`, `insurance`, `pickedUp`, `originAddress`, `destAddress`) 
+	VALUES ('', '$first','$last', 'Normal', '$email','$date', '$phone','$time','$insurance','No','$address, $state, $zip', '$address2, $state2, $zip2')";
 	$query = mysqli_query($db_conx, $sql); 
 	
 }
@@ -33,9 +37,6 @@ if(isset($_POST["first"])){
 <form id="pickUpForm" name="normalPickupForm" method="post" action="index.php">
 <div class="abc" style="margin-top:10px">Please fill out the information below</div>
  <div id="signupFormDiv">
-	<label><b>Shipment ID you wish to pick up</b></label><br>
-    <input id="shipmentId" type="text" placeholder="Shipment ID" name="shipmentId" required maxlength="15"><br>
-    
  <label><b>First</b></label><br>
     <input id="first" type="text" placeholder="First" name="first" required maxlength="30"><br>
     <label><b>Last</b></label><br>
@@ -48,15 +49,20 @@ if(isset($_POST["first"])){
 	<input type="time" name="tm"><br/>
 	<label><b>Phone Number</b></label><br>
     <input id="phone" type="text" placeholder="Phone Number" name="phone" required maxlength="15"><br>
-    <label><b>Choose a location to pickup from</b></label><br>
-    <select id="address" name="address">
-  <option value="7 day ground economy shipping">5154 State University Dr, Los Angeles, CA 90032</option>
-  <option value="Next day air expedited shipping">University of California, Los Angeles, Los Angeles, CA 90095</option>
-  <option value="2 day air expedited shipping">Gold's Gym, 735 S Figueroa St #100, Los Angeles, CA 90017</option>
-  <option value="4 day ground shipping">1 World Way, Los Angeles, CA 90045</option>
-  <option value="International air economy shipping">University of Southern California, Los Angeles, CA 90007</option>
-  <option value="International air expedited 2 day shipping">LA Fitness, 1833 South La Cienega Boulevard, Los Angeles, CA 90035</option>
-</select><br>
+    <label><b>Your Address</b></label><br>
+    <input id="address" type="text" placeholder="Street Address" name="address" required maxlength="120" size="40">
+	<select name="country" id="country">
+      <?php include("../country_list.php"); ?>
+    </select>
+	<input id="state" type="text" placeholder="State" name="state" required maxlength="4">
+	  <input id="zip" type="text" placeholder="Zip code" name="zip" required maxlength="9"><br>
+	<label><b>Destination Address</b></label><br>
+    <input id="address" type="text" placeholder="Street Address" name="address2" required maxlength="120" size="40">
+	<select name="country2" id="country">
+      <?php include("../country_list.php"); ?>
+    </select>
+	 <input id="state" type="text" placeholder="State" name="state2" required maxlength="4">
+	  <input id="zip" type="text" placeholder="Zip code" name="zip2" required maxlength="9"><br>
 	<label><b>Insurance</b></label><br>
 	<select name="insurance" id="insurance">
 	<option value="yes">Yes</option>
